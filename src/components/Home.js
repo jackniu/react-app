f33c8c1f946f5b14e6eeaecf3e3e86d536f025ad
jackniu <chuangnzh@qq.com>
2019-05-25 12:10:54 +0800

    Change folder structure.

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+    // constructor() {
+    //     super();
+    // }
+
+    render() {
+        return <div>Home 组件</div>
+    }
+}
+
+export default Home;
