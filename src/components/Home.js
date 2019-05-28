7e12fcc3294d2c77d427734b05d258c5b946aa67
jackniu <chuangnzh@qq.com>
2019-05-28 16:31:17 +0800

    get state data

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,23 +1,34 @@
 import React, { Component } from 'react';
+import '../assets/css/index.css';
 
 class Home extends Component {
     constructor() {
         super();
         this.state = {
-            title: 'This is my name',
+            title: 'This is my title',
             user: {
                 name: 'Jane',
                 age: 20
+            },
+            style: {
+                fontSize: '24px',
+                color: 'green'
             }
         }
     }
 
     render() {
         return (
-            <div>
+            <div className="home">
                 <h1>Name：{this.state.title}</h1>
                 <p>{this.state.user.name}</p>
                 <p>{this.state.user.age}</p>
+                <div className="red">test</div>
+                <label htmlFor="name">Name</label>
+                <input id="name" type="text" />
+                <div style={{ color: 'gray' }}>灰色</div>
+                <div style={this.state.style}>绿色</div>
+                <img className="logo" src={require('../assets/images/logo.svg')} />
             </div>
         )
     }
