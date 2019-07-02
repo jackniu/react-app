b503d7b3bf04399448aff61a23ed0e5288a42e70
jackniu <chuangnzh@qq.com>
2019-07-02 19:56:23 +0800

    Render array data into html

--- src/components/News.js
+++ src/components/News.js
@@ -5,8 +5,22 @@ import demo_image from '../assets/images/demo.jpg'
 class News extends React.Component {
     constructor(props) {
         super(props); /* props 用于父子组件传值 固定写法*/
+
+        this.state = {
+            list1: ['111', '222', '333'],
+            list2: [<h3 key="1">test1</h3>, <h3 key="2">test2</h3>],
+            list3: [
+                { title: '新闻1' },
+                { title: '新闻2' },
+                { title: '新闻3' },
+                { title: '新闻4' }
+            ]
+        }
     }
     render() {
+        let mylist1 = this.state.list1.map(function (value, key) {
+            return <li key={key}>{value}</li>
+        })
         return (
             <div>
                 <ul>
@@ -18,7 +32,16 @@ class News extends React.Component {
                     <img src={require('../assets/images/demo.jpg')} alt="" />
                     <img src="https://tse1-mm.cn.bing.net/th?id=OET.4b3808faa5fc4b1ab6d9e10763ed67fd&w=272&h=272&c=7&rs=1&o=5&pid=1.9" alt="" />
                 </div>
-                <div></div>
+                <div>{this.state.list2}</div>
+                <div>{this.state.list1}</div>
+                <ul>{mylist1}</ul>
+                <ul>
+                    {
+                        this.state.list3.map(function (value, key) {
+                            return <li key={key}>{value.title}</li>
+                        })
+                    }
+                </ul>
             </div>
         )
     }
