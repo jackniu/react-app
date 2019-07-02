bba48ec7f80f411c5447f20f69b57e900c470522
jackniu <chuangnzh@qq.com>
2019-07-02 19:32:18 +0800

    Add internet img

--- src/components/News.js
+++ src/components/News.js
@@ -16,6 +16,7 @@ class News extends React.Component {
                 </ul>
                 <div className="myimg"><img src={demo_image} alt="" />
                     <img src={require('../assets/images/demo.jpg')} alt="" />
+                    <img src="https://tse1-mm.cn.bing.net/th?id=OET.4b3808faa5fc4b1ab6d9e10763ed67fd&w=272&h=272&c=7&rs=1&o=5&pid=1.9" alt="" />
                 </div>
                 <div></div>
             </div>
