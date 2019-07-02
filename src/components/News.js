cee94a964809155a189c93e64784b1e73be40653
jackniu <chuangnzh@qq.com>
2019-07-02 19:14:40 +0800

    Add demo image.

--- src/components/News.js
+++ src/components/News.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import '../assets/css/index.css';
+import demo_image from '../assets/images/demo.jpg'
 
 class News extends React.Component {
     constructor(props) {
@@ -12,6 +14,10 @@ class News extends React.Component {
                     <li>列表</li>
                     <li>列表</li>
                 </ul>
+                <div className="myimg"><img src={demo_image} alt="" />
+                    <img src={require('../assets/images/demo.jpg')} alt="" />
+                </div>
+                <div></div>
             </div>
         )
     }
